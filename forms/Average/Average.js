fe705ba36db20347ca36f0e7be1e67b83df39517
altaylor928 <60445248+altaylor928@users.noreply.github.com>
2020-02-05 18:02:09 -0600

    Update Average.js

--- forms/Average/Average.js
+++ forms/Average/Average.js
@@ -1,4 +1,4 @@
-//asks user for sum or average and displays
+//asks user for sum or average and displays the sum or average 
 let myNumbers = [34, 56, 117, 324, 183, 231, 72, 111, -10, 207]
 let question1 = prompt("Do you want to sum or average the numbers?")
 let sumNumbers = 34 + 56 + 117 + 324 + 183 + 231 + 72 + 111 + -10 + 207
@@ -15,22 +15,21 @@ switch (question1) {
     break;
     }
 
-//asks user for additional numbers and adds 10 to the last number
-//asks for first number and then adds 10
+//asks user for additional numbers
+//asks for first number
 let getANumber = prompt("Please enter a number: ")
-myNumbers.push(getANumber)
-let numberTen = 10
-lastNum = myNumbers.slice(-1)
-alert(`The last number in the array plus 10 is ${lastNum + numberTen}.`)
+  myNumbers.push(getANumber)
 
-//asks for second number and then adds 10
+//asks for second number
 let getANumber2 = prompt("Please enter another number: ")
-myNumbers.push(getANumber2)
-lastNum2 = myNumbers.slice(-1)
-alert(`The last number in the array plus 10 is ${lastNum2 + numberTen}.`)
+  myNumbers.pop(getANumber2)
 
-//asks for third number and then adds 10 
+//asks for third number
 let getANumber3 = prompt("Please enter another number: ")
-myNumbers.push(getANumber3)
-lastNum3 = myNumbers.slice(-1)
-alert(`The last number in the array plus 10 is ${lastNum3 + numberTen}.`)
+  myNumbers.push(getANumber3)
+
+//shows last number + 10
+let lastNum = Number(myNumbers.pop())
+  alert(`The current last number in the array plus 10 is ${lastNum + 10}.`)
+
+
